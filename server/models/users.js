2dfba13967e49c9216120b986f950c414db9d018
subham0269 <gitsubham21@gmail.com>
2024-07-02 23:20:38 +0530

    Add password hashing using bcrypt

--- server/models/users.js
+++ server/models/users.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcrypt";
+import dotenv from 'dotenv';
+dotenv.config();
 const userSchema = mongoose.Schema({
    username: {
       type: String,
@@ -18,6 +20,21 @@ const userSchema = mongoose.Schema({
 
 }, {timestamps: true})
 
+userSchema.pre('save', async function (next)  {
+   try {
+      const salt = await bcrypt.genSalt(9);
+      this.password = await bcrypt.hash(this.password, salt);
+   } catch (err) {
+      console.log('error in user schema middleware', err);
+   } finally {
+      next();
+   }
+   
+   console.log(hash);
+   next();
+   // bcrypt.hash(this.password, salt_roundes)
+})
 const userModel = mongoose.model('user-auth', userSchema);
+
 
 export default userModel;
